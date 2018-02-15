a443ee7a74bd43340c275bf33e6f404e66107e40
cyperghost <crcerror@users.noreply.github.com>
2018-02-15 19:15:27 +0100

    0.50 PR
    That's a massive update now!
    
    0.50
    Added Multifile selection
    Toggle File Selection
    
    This is now a PR = Productive Release

--- BashROMManager.sh
+++ BashROMManager.sh
@@ -5,7 +5,8 @@
 # 07/02/18 - 0.2 Per System selection, no relase
 # 12/02/18 - 0.3 Detect empty directories, no release
 # 13/02/18 -0.31 Full console Names in array, Loop function
-# 
+# 15/02/18 - 0.5 Multiple selection possible
+#
 # This will let you delete files in specific ROM folders
 # This script is best called into RetroPie Menu
 # Into the menu choose 'BashROMManager'
@@ -17,7 +18,6 @@
     [ -z "${rom_dir##*/}" ] && rom_dir="${rom_dir%?}"
 
 # Folder Array
-
     folder_array=("$rom_dir"/*/)
     folder_array=("${folder_array[@]%?}")
     folder_array=("${folder_array[@]##*/}")
@@ -32,15 +32,6 @@
              "sega32x" "Sega 32X" "sfc" "Nintendo Super Famicom" "snes" "Super Nintendo Entertainment System" "tg16" "TurboGrafx 16" \
              "tg16cd" "TurboGrafx 16CD" "cps1" "Capcom Play System I" "cps2" "Capcom Play System II" "cps3" "Capcom Play System III")
 
-
-function del_file() {
-    # Delete Save games
-    # Is Array value already empty? If Yes then return
-    [ -z "${file_array[$2-1]}" ] && return
-
-    dialog --yesno "I will delete following file after you choose YES\n\n$1\n" 10 60
-    [ $? = 0 ] && rm -f "$1" && file_array[$2-1]="" && options[$2*2-1]=""
-}
 
 function contains_element () {
     # Search array for entry
@@ -69,57 +60,116 @@ function folder_select() {
         fi
     done
 
-    local cmd=(dialog --backtitle "cyperghosts BashROMManager v0.31" \
+    local cmd=(dialog --backtitle "cyperghosts BashROMManager v0.50" \
                       --title " Systemselection " \
                       --cancel-label "Exit to ES" \
                       --menu "Available systems:" 16 70 16)
     local choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+
     rom_sysdir="$rom_dir/$choices"
 }
 
-# --- MAIN ---
+# --- MAIN Functions ---
+
+function del_files() {
+    # Delete ROM-files
+    local e
+    for e in "${del_array[@]}"; do
+        dialog --yesno "I will delete following file after you choose YES\n\n$e\n" 10 60
+        [ $? = 0 ] && rm -f "$e"
+    done
+}
+
+function toggle_entry() {
+    # This actives/deactives entry in file selection list
+    # If a file is located it will be set to del_array and the entry will be faded out in list
+    # If an entry is already faded out, the filename will be rebuild and entry will removed from del_arry
+    # IF part REBUILDS entries, ELSE removes file from entry and sets to file to del_array
+    #
+    # --- Terrible coded ---
+    #
+    # Sleep is for debouncing
+
+    if [[ "${options[choices*2-1]}" ]]; then 
+        del_array+=("${file_array[choices-1]}")
+        options[choices*2-1]=""
+    else
+        file_name="${file_array[choices-1]##*/}"
+        extension="${file_name##*.}"
+        options[choices*2-1]="$extension - $file_name"
+        contains_element "${file_array[choices-1]}" "${del_array[@]}"
+        unset del_array[idx]
+        del_array=("${del_array[@]}")
+    fi
+
+   sleep 0.1
+}
+
+# --- MAIN Programm --
+
+folderselect=1 #Needed to select system in first run
+
 while true
 do
+    # Save some space and empty arrays on loop
     unset options
-    folder_select
+    unset del_array
+
+    # Run System Selection on first run
+    [ $folderselect = 1 ] && folder_select
     [ -z "${rom_sysdir##*/}" ] && echo "Aborting..." && exit
+
 
     # Get Console Name
     contains_element "${rom_sysdir##*/}" "${console[@]}"
         [ $? = 0 ] && console_name="${rom_sysdir##*/} - System unknown" 
         [ $? = 1 ] && console_name="${console[idx+1]}"
 
-    # Build file Array and get Array size
+    # Build file Array for path $rom_sysdir and get Array size
         file_array=("$rom_sysdir"/*)
         idx=${#file_array[@]}
 
     # Building Choices Array for options
     # Means "counter Text counter Text"
     # The test commands hinder to choose directories!
-    # A file without '.' is likly a directory
+    # A file without '.' is likely a directory
         for (( z=0; z<$idx; z++ ))
         do
-            name="${file_array[z]##*/}"
-            ext="${name##*.}"
-            [ "$ext" != "$name" ] && options+=("$((z+1))" "$ext - $name")
+            file_name="${file_array[z]##*/}"
+            extension="${file_name##*.}"
+            [ "$extension" != "$file_name" ] && options+=("$((z+1))" "$extension - $file_name")
         done
 
     # Array validity check!
-        idx=${#options[@]}
-        [ $idx = 0 ] && dialog --title " Error " --infobox "\nLikely just a SubDirectory!\n\nExit to EmulationStation!\n" 7 35 && sleep 3 && exit
+        [ ${#options[@]} = 0 ] && dialog --title " Error " --infobox "\nLikely just a SubDirectory!\n\nExit to EmulationStation!\n" 7 35 && sleep 3 && exit
 
     # Build Dialog output of File selection
     while true
     do
-        cmd=(dialog --backtitle "cyperghosts BashROMManager v0.31" \
+        cmd=(dialog --backtitle "cyperghosts BashROMManager v0.50" \
+                    --default-item "$choices"
                     --title " Selected Console: $console_name" \
+                    --ok-label "Select Item"
                     --cancel-label "Back to SysSelection" \
-                    --menu "Select file to delete:" 17 80 16)
+                    --extra-button --extra-label "Delete ${#del_array[@]} items in queue"
+                    --menu "Select files you want to add to delete queue" 17 80 16)
         choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
 
+        status=$?
+        [ $status = 3 ] && choices="E"
+        [ $status = 1 ] && choices="B"
+
         case $choices in
-            [1-9999]) del_file "${file_array[choices-1]}" "$choices" ;;
-            *) break ;;
+           [1-9999]*) toggle_entry
+                      ;;
+                   E) folderselect=0 
+                      [ ${#del_array[@]} = 0 ] && return
+                      del_files; break 
+                      ;;
+                   B) folderselect=1; break
+                      ;;
+                   *) exit 1
+                      ;;
         esac
     done
 done
